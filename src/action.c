0fa81e01a1d2076096b004033a0de4746aabea7e
eziod <ezio.decadt@epitech.eu>
2024-03-12 10:12:00 +0100

    [~] Add check to prevent write of letter in input size

--- src/action.c
+++ src/action.c
@@ -39,7 +39,7 @@ void write_for_popup(my_paint_t *my_paint, sfEvent event)
         len = my_strlen(my_paint->window.popup_text_str);
         if (event.text.unicode == '\b' && len > 0)
             my_paint->window.popup_text_str[len - 1] = '\0';
-        if (len < 2 && event.text.unicode >= 32 && event.text.unicode <= 126) {
+        if (len < 2 && my_isdigit(event.text.unicode)) {
             my_paint->window.popup_text_str[len] = event.text.unicode;
             my_paint->window.popup_text_str[len + 1] = '\0';
         }
@@ -47,7 +47,7 @@ void write_for_popup(my_paint_t *my_paint, sfEvent event)
             my_paint->window.popup_text_str);
     }
     if (event.type == sfEvtKeyPressed && event.key.code == sfKeyReturn) {
-        my_paint->tools.size = my_atoi(my_paint->window.popup_text_str);
+        my_paint->tools.size = atoi(my_paint->window.popup_text_str);
         my_paint->window.popup_text_str[0] = '\0';
     }
 }
